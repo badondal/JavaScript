6c5293cbc8ee7fc04b55cc9ad5f58e884ee85d08
badondal <badondal@naver.com>
2018-10-18 09:46:41 +0900

    20181017

---
+++ script.js
@@ -0,0 +1,6 @@
+
+        var hello = document.getElementById('hello')
+        hello.addEventListener('click', function(){
+            alert('안녕')
+
+        })
---
+++ script2.js
@@ -0,0 +1,6 @@
+window.onload =function(){
+    var hi = document.getElementById('hello')
+    hi.addEventListener('click', function(){
+        alert('hello world');
+    })
+}
